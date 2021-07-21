3d3d4fa75f43ab5c81299a914ca9e1e451cc86f4
Samir Góes <samir.goes@gmail.com>
2021-07-21 17:07:48 -0300

    Commit inicial

---
+++ src/GraphQl.Api/Entities/Produto.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GraphQl.Api.Entities
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public decimal Preco { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string ImagemUrl { get; set; }
+
+        public float Estoque { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public int CategoriaId { get; set; }
+    }
+}
